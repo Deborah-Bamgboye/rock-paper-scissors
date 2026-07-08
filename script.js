00037dd592895cae868597cd86da86dfcc3d2e33
Deborah-Bamgboye <bama15299@gmail.com>
2026-07-08 14:12:27 +0100

    logic for rock-paper-scissors engine

---
+++ script.js
@@ -0,0 +1,69 @@
+console.log("Hello World")
+let options = ["rock","paper","scissors"]
+function getComputerChoice() {
+let num = Math.floor(Math.random() * 3)
+ 
+return options[num]
+    
+}
+//initialise the array with the three values
+// use math.random to generate a random index for the array
+//  which the engine will return as the value for choice
+function getHumanChoice() {
+
+    return (prompt("rock,paper or scissors")).toLowerCase()
+}
+let humanScore = 0
+let computerScore = 0
+
+
+
+
+function playRound(humanChoice, computerChoice){
+// if they are both the same
+
+switch (true) {
+    case (humanChoice === computerChoice):
+    // Code to run if expression === value1
+    console.log("No one winsz, computer chose" )
+    break;
+    case (humanChoice === "rock" && computerChoice === "paper"):
+    console.log("You lose, computer chose" )
+    computerScore++ 
+    // Code to run if expression === value2
+    break;
+    case (humanChoice === "paper" && computerChoice === "rock"):
+        console.log("You win, computer chose ")
+        humanScore++
+    break;
+    case (humanChoice === "paper" && computerChoice === "scissors"):
+            console.log("You lose, computer chose ")
+            computerScore++
+    break;
+    case (humanChoice === "scissors" && computerChoice === "paper"):
+            console.log("You win, computer chose " )
+            humanScore++
+    break;
+    case (humanChoice === "scissors" && computerChoice === "rock"):
+            console.log("You lose, computer chose " )
+            computerScore++
+    break;
+    case (humanChoice === "rock" && computerChoice === "scissors"):
+            console.log("You win, computer chose " )
+            humanScore++
+    break;
+}
+}
+// const humanSelection = getHumanChoice;
+// const computerSelection = getComputerChoice;
+
+
+function playGame() {
+    for (let i = 0; i < 5.; i++) {
+        playRound(getHumanChoice(), getComputerChoice());
+     
+       
+    }
+   console.log("Your score: " + humanScore + "," + "computer's: " + computerScore) 
+}
+playGame()
